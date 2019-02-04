857d0e4cfcaf2cf5afbbd5bf2e7fefa76379883c
Al-1010 <al.xion.1010@gmail.com>
2019-02-04 12:37:53 +0900

    First commit

---
+++ hyoi/hyoi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hyoi
+//
+//  Created by 加藤雄大 on 2019/02/04.
+//  Copyright © 2019年 加藤雄大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
